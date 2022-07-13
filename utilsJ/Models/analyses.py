5b1470f522befffafd106570ef5a45fa68fbed9b
manuelmolano <manuelmolanomazon@gmail.com>
2022-07-13 14:27:33 +0200

    PEP8

--- utilsJ/Models/analyses.py
+++ utilsJ/Models/analyses.py
@@ -3,26 +3,31 @@
 Created on Wed Jul 13 12:47:49 2022
 @author: Alex Garcia-Duran
 """
-import os
-from sklearn.model_selection import ParameterGrid
-from utilsJ.regularimports import *
-from utilsJ.Behavior import plotting, ComPipe
-from utilsJ.Models import traj, simul
-from concurrent.futures import as_completed, ThreadPoolExecutor
-from scipy.stats import ttest_ind, sem
-from matplotlib import cm
-import swifter
-import seaborn as sns
-from scipy.stats import norm
-import warnings
+import numpy as np
+from utilsJ.Behavior import plotting
+from utilsJ.Models import traj
+import matplotlib.pyplot as plt
+
+# import os
+# from sklearn.model_selection import ParameterGrid
+# from utilsJ.regularimports import *
+# import pandas as pd
+# from concurrent.futures import as_completed, ThreadPoolExecutor
+# from scipy.stats import ttest_ind, sem
+# from matplotlib import cm
+# import swifter
+# import seaborn as sns
+# from scipy.stats import norm
+# import warnings
 general_path = traj.general_path
 
 
 def com_matrix_comparison_silent_vs_nonsilent(df):
     # comparing CoM matrix for silent vs non-silent samples
-    subset=df.dropna(subset=["avtrapz", "allpriors", "CoM_sugg", "special_trial"])
-    subset_silent=subset.loc[subset.special_trial==2]
-    subset_nonsilent=subset.loc[subset.special_trial==0]
+    subset = df.dropna(
+        subset=["avtrapz", "allpriors", "CoM_sugg", "special_trial"])
+    subset_silent = subset.loc[subset.special_trial == 2]
+    subset_nonsilent = subset.loc[subset.special_trial == 0]
     mat_data_silent, _ = plotting.com_heatmap(
         subset_silent.allpriors,
         subset_silent.avtrapz,
@@ -36,7 +41,7 @@ def com_matrix_comparison_silent_vs_nonsilent(df):
     plt.figure()
     plt.plot(mat_data_silent)
     plt.plot(mat_data_nonsilent)
-    
+
     return mat_data_silent, mat_data_nonsilent
 
 
@@ -44,16 +49,17 @@ def pCoM_vs_coh(df, subject):
     """
     It computes the pCoM vs the coherence
     """
-    ev_vals = np.sort(np.abs(df.coh2.unique())) # unique absolute value coherence values
+    ev_vals = np.sort(np.abs(df.coh2.unique()))  # unique absolute value coh values
     if subject == 'all':
         all_pcom_coh = np.zeros((len(df.subjid.unique()), len(ev_vals)+1))
         for i, subj in enumerate(df.subjid.unique()):
             df_sub = df.loc[df.subjid == subj]
-            silent = df_sub.loc[df_sub.special_trial == 2] # separating silent from nonsilent
+            # separating silent from nonsilent
+            silent = df_sub.loc[df_sub.special_trial == 2]
             nonsilent = df_sub.loc[df_sub.special_trial == 0]
-            pcom_coh = []  #np.mean(silent.CoM_sugg)
+            pcom_coh = []  # np.mean(silent.CoM_sugg)
             num = np.array([len(silent)])
-            for ev in ev_vals: # for each coherence, a pCoM for each subject
+            for ev in ev_vals:  # for each coherence, a pCoM for each subject
                 index = np.abs(df.coh2) == ev
                 pcom_coh.append(np.nanmean(nonsilent.CoM_sugg[index]))
                 num = np.concatenate([num, np.array([sum(index)])])
