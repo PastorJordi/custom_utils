0f5cbb5b27bb0852b3ce4801c932fed54d2058e4
manuelmolano <manuelmolanomazon@gmail.com>
2022-07-19 18:41:46 +0200

    tune figures, stored proactive/reactive trials, remove unnecesary variables

--- utilsJ/Models/extended_ddm.py
+++ utilsJ/Models/extended_ddm.py
@@ -11,108 +11,53 @@ import matplotlib.pyplot as plt
 # ddm
 
 
-def plotting(com, E, second_ind, first_ind):
-    f, ax = plt.subplots(nrows=4, ncols=2)
+def draw_lines(ax, frst, sec, p_t_eff):
+    ax[0].axhline(y=1, color='purple', linewidth=2)
+    ax[0].axhline(y=-1, color='green', linewidth=2)
+    ax[0].axhline(y=0, linestyle='--', color='k', linewidth=0.7)
+    ax[0].axhline(y=0.5, color='purple', linewidth=1, linestyle='--')
+    ax[0].axhline(y=-0.5, color='green', linewidth=1, linestyle='--')
+    ax[1].axhline(y=1, color='k', linewidth=1, linestyle='--')
+    ax[0].axvline(x=frst, color='c', linewidth=1, linestyle='--')
+    ax[1].axvline(x=frst, color='c', linewidth=1, linestyle='--')
+    if sec < frst+p_t_eff:
+        ax[0].axvline(x=sec, color='c', linewidth=1, linestyle='--')
+        ax[1].axvline(x=sec, color='c', linewidth=1, linestyle='--')
+
+
+def plotting(com, E, second_ind, first_ind, resp_first, resp_fin, pro_vs_re,
+             p_t_eff, trial=0):
+    f, ax = plt.subplots(nrows=4, ncols=2, figsize=(8, 12))
     ax = ax.flatten()
-    indx_no_com = np.where(~com)[0]
-    # ax.plot(E[:, indx_no_com], 'k')
-    # ax.plot(E[:first_ind[indx_no_com]+t_motor, indx_no_com], 'r')
-    # ax.plot(E[:first_ind[indx_no_com]+1, indx_no_com], 'b')
-    indx_com = np.where(com)[0]
-    # for i_c in indx_com:
-    # ax.plot(E[:, indx_com], '--k')
     ax[0].set_title('CoM')
     ax[1].set_title('No-CoM')
     ax[6].set_xlabel('Time (ms)')
     ax[7].set_xlabel('Time (ms)')
-    if E[first_ind[indx_com[4]], indx_com[4]] < 0:
-        col = 'green'
-    else:
-        col = 'purple'
-    ax[0].scatter(first_ind[indx_com[4]], E[first_ind[indx_com[4]],
-                                            indx_com[4]],
-                  color=col, linewidths=3, zorder=4)
-    if E[second_ind[indx_com[4]], indx_com[4]] < 0:
-        col = 'green'
-    else:
-        col = 'purple'
-    ax[0].scatter(second_ind[indx_com[4]],
-                  E[second_ind[indx_com[4]], indx_com[4]],
-                  color=col, linewidths=3, zorder=4)
-    ax[0].plot(E[:first_ind[indx_com[4]]+t_motor+1, indx_com[4]], 'r')
-    ax[0].plot(E[:first_ind[indx_com[4]]+1, indx_com[4]], 'b')
-    ax[2].plot(A[:first_ind[indx_com[4]]+t_motor+1, indx_com[4]], 'r')
-    ax[2].plot(A[:first_ind[indx_com[4]]+1, indx_com[4]], 'b')
-    ax[1].plot(E[:first_ind[indx_no_com[12]]+t_motor+1, indx_no_com[12]], 'r',
-               label='After hitting')
-    ax[1].plot(E[:first_ind[indx_no_com[12]]+1, indx_no_com[12]], 'b',
-               label='Before hitting')
-    if E[first_ind[indx_no_com[12]], indx_no_com[12]] < 0:
-        col = 'green'
-    else:
-        col = 'purple'
-    ax[1].scatter(first_ind[indx_no_com[12]], E[first_ind[indx_no_com[12]],
-                                                indx_no_com[12]],
-                  color=col, linewidths=3, zorder=4)
-    if E[first_ind[indx_no_com[12]]+t_motor, indx_no_com[12]] < 0:
-        col = 'green'
-    else:
-        col = 'purple'
-    ax[1].scatter(first_ind[indx_no_com[12]]+t_motor,
-                  E[first_ind[indx_no_com[12]]+t_motor, indx_no_com[12]],
-                  color=col, linewidths=3, zorder=4)
-    ax[1].legend()
-    ax[3].plot(A[:first_ind[indx_no_com[12]]+t_motor+1, indx_no_com[12]], 'r')
-    ax[3].plot(A[:first_ind[indx_no_com[12]]+1, indx_no_com[12]], 'b')
-    ax[4].plot(E[:first_ind[indx_com[1]]+t_motor+1, indx_com[1]], 'r')
-    ax[4].plot(E[:first_ind[indx_com[1]]+1, indx_com[1]], 'b')
-    if E[first_ind[indx_com[1]], indx_com[1]] < 0:
-        col = 'green'
-    else:
-        col = 'purple'
-    ax[4].scatter(first_ind[indx_com[1]], E[first_ind[indx_com[1]], indx_com[1]],
-                  color=col, linewidths=3, zorder=4)
-    if E[second_ind[indx_com[1]], indx_com[1]] < 0:
-        col = 'green'
-    else:
-        col = 'purple'
-    ax[4].scatter(second_ind[indx_com[1]],
-                  E[second_ind[indx_com[1]], indx_com[1]],
-                  color=col, linewidths=3, zorder=4)
-    ax[6].plot(A[:first_ind[indx_com[1]]+t_motor+1, indx_com[1]], 'r')
-    ax[6].plot(A[:first_ind[indx_com[1]]+1, indx_com[1]], 'b')
-    ax[5].plot(E[:first_ind[indx_no_com[0]]+t_motor+1, indx_no_com[0]], 'r')
-    ax[5].plot(E[:first_ind[indx_no_com[0]]+1, indx_no_com[0]], 'b')
-    if E[first_ind[indx_no_com[0]], indx_no_com[0]] < 0:
-        col = 'green'
-    else:
-        col = 'purple'
-    ax[5].scatter(first_ind[indx_no_com[0]], E[first_ind[indx_no_com[0]],
-                                               indx_no_com[0]],
-                  color=col, linewidths=3, zorder=4)
-    if E[first_ind[indx_no_com[0]]+t_motor, indx_no_com[0]] < 0:
-        col = 'green'
-    else:
-        col = 'purple'
-    ax[5].scatter(first_ind[indx_no_com[0]]+t_motor,
-                  E[first_ind[indx_no_com[0]]+t_motor, indx_no_com[0]],
-                  color=col, linewidths=3, zorder=4)
-    ax[7].plot(A[:first_ind[indx_no_com[0]]+t_motor+1, indx_no_com[0]], 'r')
-    ax[7].plot(A[:first_ind[indx_no_com[0]]+1, indx_no_com[0]], 'b')
-    ax[0].set_ylabel('EA (Proactive)')
-    ax[1].set_ylabel('EA (Proactive)')
-    ax[2].set_ylabel('AI (Proactive)')
-    ax[3].set_ylabel('AI (Proactive)')
-    ax[4].set_ylabel('EA (Reactive)')
-    ax[5].set_ylabel('EA (Reactive)')
-    ax[6].set_ylabel('AI (Reactive)')
-    ax[7].set_ylabel('AI (Reactive)')
-    for i in range(ax.shape[0]):
-        ax[i].axhline(y=1, color='purple', linewidth=2)
-        ax[i].axhline(y=-1, color='green', linewidth=2)
-        ax[i].axhline(y=0, linestyle='--', color='k', linewidth=0.7)
-        ax[i].axhline(y=0.5, color='purple', linewidth=1, linestyle='--')
-        ax[i].axhline(y=-0.5, color='green', linewidth=1, linestyle='--')
+    axes = [[0, 2], [1, 3], [4, 6], [5, 7]]
+    trials = [0, 0, 1, 1]
+    mat_indx = [np.logical_and(com, pro_vs_re == 0),
+                np.logical_and(~com, pro_vs_re == 0),
+                np.logical_and(com, pro_vs_re == 1),
+                np.logical_and(~com, pro_vs_re == 1)]
+    y_lbls = ['CoM Proactive', 'No CoM Proactive', 'CoM Reactive',
+              'No CoM Reactive']
+    for i, (a, t, m, l) in enumerate(zip(axes, trials, mat_indx, y_lbls)):
+        trial = np.where(m)[0][t]
+        draw_lines(ax[np.array(a)], frst=first_ind[trial], sec=second_ind[trial],
+                   p_t_eff=p_t_eff)
+        color1 = 'green' if resp_first[trial] < 0 else 'purple'
+        color2 = 'green' if resp_fin[trial] < 0 else 'purple'
+
+        ax[a[0]].plot(E[:first_ind[trial]+p_t_eff+1, trial], color=color2,
+                      alpha=0.7)
+        ax[a[0]].plot(E[:first_ind[trial]+1, trial], color=color1, lw=2)
+        ax[a[1]].plot(A[:first_ind[trial]+p_t_eff+1, trial], color=color2,
+                      alpha=0.7)
+        ax[a[1]].plot(A[:first_ind[trial]+1, trial], color=color1, lw=2)
+        ax[a[0]].set_ylim([-1.5, 1.5])
+        ax[a[1]].set_ylim([-0.1, 1.5])
+        ax[a[0]].set_ylabel(l+' EA')
+        ax[a[1]].set_ylabel(l+' AI')
 
 
 def trial_ev_vectorized(zt, stim, p_w_zt, p_w_stim, p_e_noise, p_com_bound,
@@ -137,6 +82,7 @@ def trial_ev_vectorized(zt, stim, p_w_zt, p_w_stim, p_e_noise, p_com_bound,
     # reaction_time = 300
     first_ind = []
     second_ind = []
+    pro_vs_re = []
     resp_first = np.ones(E.shape[1])
     resp_fin = np.ones(E.shape[1])
     for i_c in range(E.shape[1]):
@@ -149,6 +95,7 @@ def trial_ev_vectorized(zt, stim, p_w_zt, p_w_stim, p_e_noise, p_com_bound,
         if (indx_hit_action).any():
             hit_action = np.where(indx_hit_action)[0][0]
         hit_dec = min(hit_bound, hit_action)
+        pro_vs_re.append(np.argmin([hit_action, hit_bound]))
         first_ind.append(hit_dec)
         # first response
         resp_first[i_c] *= (-1)**(E[hit_dec, i_c] < 0)
@@ -165,17 +112,13 @@ def trial_ev_vectorized(zt, stim, p_w_zt, p_w_stim, p_e_noise, p_com_bound,
         second_ind.append(indx_update_ch)
     com = resp_first != resp_fin
     first_ind = np.array(first_ind).astype(int)
-    return E, A, com, first_ind, second_ind, resp_first, resp_fin
+    pro_vs_re = np.array(pro_vs_re)
+    return E, A, com, first_ind, second_ind, resp_first, resp_fin, pro_vs_re
 
 
+# --- MAIN
 if __name__ == '__main__':
     plt.close('all')
-    bound = 1
-    fluc = 2
-    bound = 1
-    dt = 1e-3
-    t_motor = 40*1e-3
-    t_motor = int(round(t_motor/(dt)))
     num_tr = 100
     plot = False
     zt = np.random.randn(num_tr)*1e-2
@@ -186,7 +129,7 @@ if __name__ == '__main__':
                                                         num_tr)*1e-1
     p_t_m = 40
     p_w_zt = 0.2
-    p_w_stim = 0.2
+    p_w_stim = 0.3
     p_e_noise = 0.2
     p_com_bound = 0.5
     Va = np.abs(np.random.randn(num_tr))*1e-1
@@ -194,11 +137,13 @@ if __name__ == '__main__':
     bound_a = 1
     p_w_a = 0.05
     p_a_noise = 0.05
-    E, A, com, first_ind, second_ind, resp_first, resp_fin =\
+    E, A, com, first_ind, second_ind, resp_first, resp_fin, pro_vs_re =\
         trial_ev_vectorized(zt=zt, stim=stim, p_w_zt=p_w_zt,
                             p_w_stim=p_w_stim, p_e_noise=p_e_noise,
                             p_com_bound=p_com_bound,
                             p_t_m=p_t_m, p_t_eff=p_t_eff, p_t_a=p_t_a,
                             num_tr=num_tr, p_w_a=p_w_a, p_a_noise=p_a_noise,
                             plot=False)
-    plotting(E=E, com=com, second_ind=second_ind, first_ind=first_ind)
+    plotting(E=E, com=com, second_ind=second_ind, first_ind=first_ind,
+             resp_first=resp_first, resp_fin=resp_fin, pro_vs_re=pro_vs_re,
+             p_t_eff=p_t_eff)
