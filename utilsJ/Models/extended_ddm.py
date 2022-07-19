7d5ed1431594a38c1fbae26638e6e0f164efc0ff
manuelmolano <manuelmolanomazon@gmail.com>
2022-07-19 15:15:49 +0200

    change name of file

--- utilsJ/Models/extended_ddm.py
+++ utilsJ/Models/extended_ddm.py
@@ -9,118 +9,8 @@ import matplotlib.pyplot as plt
 # import scipy as sp
 
 # ddm
-<<<<<<< HEAD
 
 
-def evidence(offset, Ve, fluc, bound, dt, t_motor, plot=False):
-    trial_dur = 1  # trial duration (s)
-    N = int(trial_dur/dt)  # number of timesteps
-    dW = []
-    W = []
-    E = []
-    dW[0] = offset + np.sqrt(dt)*(np.random.randn()*fluc+Ve)
-    W[0] = dW[0]
-    E[0] = W[0]
-    factor = 1/N
-    timesteps_motor = round(t_motor/(dt))
-    first = False
-    counter = 1
-    com = False
-    reaction_time = 300
-    for i in range(1, N):
-        dW[i] = np.sqrt(dt)*(np.random.randn()*fluc+Ve)
-        W[i] = dW[i] + W[i-1]
-        E[i] = W[i]
-        if np.abs(E[i]) >= bound and not first:
-            first = True
-            stop_var = i
-            if E[i] > 0:
-                # print('initial: right')
-                response_in = 1
-            else:
-                # print('initial: left')
-                response_in = -1
-            # if plot:
-            #     plt.figure()
-            #     plt.plot(np.linspace(0, stop_var, num=stop_var)*factor, E[0:i])
-            #     plt.ylabel('Evidence Accumulation')
-            #     plt.xlabel('Time (s)')
-        if i == N-1:
-            com = False
-            return E, reaction_time, com
-        if first:
-            counter += 1
-            if counter == timesteps_motor:
-                # if plot:
-                # plt.plot(np.linspace(stop_var-1, stop_var+timesteps_motor-1,
-                #                   num=timesteps_motor)*factor,
-                #       E[stop_var-1:stop_var+timesteps_motor-1])
-                # plt.axhline(1, linestyle='--', color='k', linewidth=0.4)
-                # plt.axhline(0, linestyle='--', color='red', linewidth=0.4)
-                # plt.axhline(-1, linestyle='--', color='k', linewidth=0.4)
-=======
->>>>>>> b294c5915a636a0e0147ff7c4c632f9524d84e1e
-
-
-<<<<<<< HEAD
-
-def trial_ev_vectorized(offset, Ve, fluc, bound, dt, t_motor, num_tr, plot=False):
-    trial_dur = 1  # trial duration (s)
-    N = int(trial_dur/dt)  # number of timesteps
-    dW = np.sqrt(dt)*(np.random.randn(N, num_tr)*fluc+Ve)
-    dW[0] += offset
-    E = np.cumsum(dW, axis=0)
-    com = False
-    # reaction_time = 300
-    first_ind = []
-    resp_first = np.ones(E.shape[1])
-    resp_fin = np.ones(E.shape[1])
-    for i_c in range(E.shape[1]):
-        if (np.abs(E[:, i_c]) >= bound).any():
-            hit_bound = np.where(np.abs(E[:, i_c]) >= bound)[0][0]
-            first_ind.append(hit_bound)
-            resp_first[i_c] *= ((-1)**(E[hit_bound, i_c] < 0))
-            indx_fin_ch = min(hit_bound+t_motor, E.shape[1])
-            resp_fin[i_c] *= ((-1)**(E[indx_fin_ch, i_c] < 0))
-        else:
-            first_ind.append(E.shape[0])
-            resp_first[i_c] *= resp_first[i_c]*((-1)**(E[-1, i_c] < 0))
-            resp_fin[i_c] = resp_first[i_c]
-    com = resp_first != resp_fin
-    first_ind = np.array(first_ind).astype(int)
-    return E, com, first_ind, resp_first, resp_fin
-
-#
-
-
-if __name__ == '__main__':
-    plt.close('all')
-    bound = 1
-    fluc = 1
-    bound = 1
-    dt = 1e-3
-    t_motor = 80*1e-3
-    t_motor = int(round(t_motor/(dt)))
-    num_tr = 100
-    plot = False
-    offset = np.random.randn(num_tr)*1e-2
-    Ve = np.random.randn(num_tr)*1e-5
-    E, com, first_ind, resp_first, resp_fin =\
-        trial_ev_vectorized(offset=offset, Ve=Ve, fluc=fluc,
-                            bound=bound, dt=dt, t_motor=t_motor,
-                            num_tr=num_tr, plot=plot)
-    f, ax = plt.subplots()
-    indx_no_com = np.where(~com)[0][0]
-    print(resp_first[indx_no_com])
-    print(resp_fin[indx_no_com])
-    ax.plot(E[:first_ind[indx_no_com]+t_motor, indx_no_com], 'r')
-    ax.plot(E[:first_ind[indx_no_com]+1, indx_no_com], 'b')
-    indx_com = np.where(com)[0][0]
-    print(resp_first[indx_com])
-    print(resp_fin[indx_com])
-    ax.plot(E[:first_ind[indx_com]+t_motor, indx_com], '--r')
-    ax.plot(E[:first_ind[indx_com]+1, indx_com], '--b')
-=======
 def plotting(com, E, second_ind, first_ind):
     f, ax = plt.subplots(nrows=4, ncols=2)
     ax = ax.flatten()
@@ -226,13 +116,14 @@ def plotting(com, E, second_ind, first_ind):
 
 
 def trial_ev_vectorized(zt, stim, p_w_zt, p_w_stim, p_e_noise, p_com_bound,
-                        p_t_m, p_t_eff, p_t_a, num_tr, p_w_a, p_a_noise, plot=False):
+                        p_t_m, p_t_eff, p_t_a, num_tr, p_w_a, p_a_noise,
+                        plot=False):
     bound = 1
     bound_a = 1
     prior = zt*p_w_zt
     Ve = stim*p_w_stim
     Va = p_w_a
-    trial_dur = 1  # trial duration (s)
+    # trial_dur = 1  # trial duration (s)
     N = stim.shape[0]  # int(trial_dur/dt)  # number of timesteps
     dW = np.random.randn(N, num_tr)*p_e_noise+Ve
     dA = np.random.randn(N, num_tr)*p_a_noise+Va
@@ -275,43 +166,8 @@ def trial_ev_vectorized(zt, stim, p_w_zt, p_w_stim, p_e_noise, p_com_bound,
     com = resp_first != resp_fin
     first_ind = np.array(first_ind).astype(int)
     return E, A, com, first_ind, second_ind, resp_first, resp_fin
->>>>>>> b294c5915a636a0e0147ff7c4c632f9524d84e1e
 
-    ax.axhline(y=1, linestyle='--', color='k', linewidth=0.8)
-    ax.axhline(y=-1, linestyle='--', color='k', linewidth=0.8)
-    ax.axhline(y=0, linestyle='--', color='k', linewidth=0.4)
 
-<<<<<<< HEAD
-    import sys
-    sys.exit()
-    flucs = np.linspace(0, 5, num=50)
-    bounds = np.linspace(0, 1, num=50)
-    com_mat = np.zeros((len(flucs), len(bounds)))
-    rtmat = np.zeros((len(flucs), len(bounds)))
-    for i in range(len(flucs)):
-        for j in range(len(bounds)):
-            com_list = []
-            react_time = []
-            for num in range(50):
-                _, rt, com = evidence(
-                    0, 0.2, flucs[i], bounds[j], 1e3, 80*1e-3)
-                com_list.append(com)
-                react_time.append(rt)
-                com_mat[j, i] = np.mean(com_list)
-                rtmat[j, i] = np.nanmean(react_time)
-    np.save('CoM_state_space_fluc_drift_0off.npy', com_mat)
-    plt.contourf(flucs, bounds, com_mat)
-    plt.xlabel("Fluctuation, $\sigma_{E}$", fontsize=14)
-    plt.ylabel('Bound, $\\theta_{E}$', fontsize=14)
-    plt.colorbar()
-    plt.title('pCoM', fontsize=16)
-    plt.figure()
-    plt.contourf(flucs, bounds, rtmat)
-    plt.xlabel("Fluctuation, $\sigma_{E}$", fontsize=14)
-    plt.ylabel('Bound, $\\theta_{E}$', fontsize=14)
-    plt.colorbar()
-    plt.title('RT(s)', fontsize=16)
-=======
 if __name__ == '__main__':
     plt.close('all')
     bound = 1
@@ -326,13 +182,13 @@ if __name__ == '__main__':
     p_t_eff = 40
     p_t_a = p_t_eff
     num_timesteps = 1000
-    stim = np.random.randn(num_tr)*1e-3+np.random.randn(num_timesteps+p_t_eff
-                                                        , num_tr)*1e-1
+    stim = np.random.randn(num_tr)*1e-3+np.random.randn(num_timesteps+p_t_eff,
+                                                        num_tr)*1e-1
     p_t_m = 40
     p_w_zt = 0.2
     p_w_stim = 0.2
     p_e_noise = 0.2
-    p_com_bound= 0.5
+    p_com_bound = 0.5
     Va = np.abs(np.random.randn(num_tr))*1e-1
     fluc_a = 0.5
     bound_a = 1
@@ -346,4 +202,3 @@ if __name__ == '__main__':
                             num_tr=num_tr, p_w_a=p_w_a, p_a_noise=p_a_noise,
                             plot=False)
     plotting(E=E, com=com, second_ind=second_ind, first_ind=first_ind)
->>>>>>> b294c5915a636a0e0147ff7c4c632f9524d84e1e
