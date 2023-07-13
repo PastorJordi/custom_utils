c609bd1af815d19b84a0726ab9c572d66918476e
alexgarciaduran <alexgaduca@gmail.com>
2023-07-13 15:41:46 +0200

    fig_version_1

--- utilsJ/paperfigs/fig_bernstein.py
+++ utilsJ/paperfigs/fig_bernstein.py
@@ -77,12 +77,16 @@ plt.rcParams.update({'ytick.labelsize': 12})
 plt.rcParams.update({'font.size': 14})
 
 def fig_bernstein(df, data_folder=DATA_FOLDER, task_img=TASK_IMG, model_img=MODEL_IMG,
-                  rat_nocom_img=RAT_noCOM_IMG, fgsz=(8, 12), inset_sz=.1,
+                  rat_nocom_img=RAT_noCOM_IMG, fgsz=(15, 12), inset_sz=.1,
                   marginx=-.04, marginy=0.1):
-    fig, ax = plt.subplots(ncols=3, nrows=4, figsize=(10, 15))
+    fig, ax = plt.subplots(ncols=4, nrows=3, figsize=fgsz)
     ax = ax.flatten()
     ax_task = ax[0]
     ax_task.axis('off')
+    ax[3].axis('off')
+    ax[2].axis('off')
+    ax[6].axis('off')
+    ax[7].axis('off')
     ax[10].axis('off')
     ax[11].axis('off')
     for a in ax:
@@ -96,26 +100,22 @@ def fig_bernstein(df, data_folder=DATA_FOLDER, task_img=TASK_IMG, model_img=MODE
     task = plt.imread(task_img)
     ax_task.imshow(task)
     # mt versus evidence panels
-    ax_mt_coh = ax[1]
-    ax_mt_zt = ax[2]
+    ax_mt_coh = ax[5]
+    ax_mt_zt = ax[4]
     # MT VS PRIOR
     df_mt = df.copy()
-    fig_1.plot_mt_vs_evidence(df=df_mt.loc[df_mt.special_trial == 2], ax=ax_mt_coh,
+    fig_1.plot_mt_vs_evidence(df=df_mt.loc[df_mt.special_trial == 2], ax=ax_mt_zt,
                               condition='choice_x_prior', prior_limit=1,
                               rt_lim=200)
     del df_mt
     # MT VS COH
     df_mt = df.copy()
-    fig_1.plot_mt_vs_evidence(df=df_mt, ax=ax_mt_zt, prior_limit=0.1,  # 10% quantile
+    fig_1.plot_mt_vs_evidence(df=df_mt, ax=ax_mt_coh, prior_limit=0.1,  # 10% quantile
                               condition='choice_x_coh', rt_lim=50)
     del df_mt
-    # REGRESSION WEIGHTS
-    df_wghts = df.copy()
-    fig_1.mt_weights(df=df_wghts, ax=ax[3], plot=True, means_errs=False)
-    del df_wghts
     # rat trajectory deeplabcut
     # tune screenshot panel
-    ax_scrnsht = ax[4]
+    ax_scrnsht = ax[1]
     ax_scrnsht.set_xticks([])
     right_port_y = 50
     center_port_y = 250
@@ -149,8 +149,8 @@ def fig_bernstein(df, data_folder=DATA_FOLDER, task_img=TASK_IMG, model_img=MODE
     ax_scrnsht.set_ylim([0, img.shape[0]])
     # trajectories
     # add insets
-    ax_zt = np.array([ax[5], ax[8]])
-    ax_cohs = np.array([ax[7], ax[6]])
+    ax_zt = np.array([ax[8], ax[10]])
+    ax_cohs = np.array([ax[9], ax[11]])
     ax_inset_1 = fp.add_inset(ax=ax_cohs[1], inset_sz=inset_sz, fgsz=fgsz,
                               marginx=marginx, marginy=marginy, right=True)
     ax_inset_1.yaxis.set_ticks_position('none')
@@ -179,27 +179,31 @@ def fig_bernstein(df, data_folder=DATA_FOLDER, task_img=TASK_IMG, model_img=MODE
                                   condition='choice_x_coh',
                                   prior_limit=0.1,  # 10% quantile
                                   cmap='coolwarm')
-
-    ax[6].cla()
-    ax[6].axis('off')
-    ax[9].cla()
-    ax[9].axis('off')
+    ax[10].cla()
+    ax[10].axis('off')
+    ax[11].cla()
+    ax[11].axis('off')
     ax_inset_1.cla()
     ax_inset_1.axis('off')
+    ax_inset.cla()
+    ax_inset.axis('off')
     # model
     ax_model = ax[6]
-    pos_ax9 = ax[9].get_position()
-    model_image = plt.imread(model_img)
-    ax_model.set_position([pos_ax9.x0, pos_ax9.y0, pos_ax9.width,
+    pos_ax9 = ax[10].get_position()
+    ax_model.set_position([pos_ax9.x0, pos_ax9.y0, pos_ax9.width*1.5,
                            pos_ax9.height*3])
+    model_image = plt.imread(model_img)
     ax_model.imshow(model_image)
+    for i in [3, 7, 11]:
+        pos = ax[i].get_position()
+        ax[i].set_position([pos.x0, pos.y0, pos.width*0.4, pos.height])
 
 
 subjects = ['LE42', 'LE43', 'LE38', 'LE39', 'LE85', 'LE84', 'LE45',
             'LE40', 'LE46', 'LE86', 'LE47', 'LE37', 'LE41', 'LE36',
             'LE44']
 # subjects = ['LE42', 'LE85', 'LE86']
-subjects = ['LE42', 'LE41']
+# subjects = ['LE42', 'LE41']
 df_all = pd.DataFrame()
 for sbj in subjects:
     df = edd2.get_data_and_matrix(dfpath=DATA_FOLDER + sbj, return_df=True,
@@ -232,13 +236,13 @@ time_trajs = edd2.get_trajs_time(resp_len=resp_len,
                                  sound_len=sound_len)
 df['time_trajs'] = time_trajs
 subjid = df.subjid.values
-print('Computing CoMs')
-time_com, peak_com, com =\
-    fig_3.com_detection(df=df, data_folder=DATA_FOLDER,
-                        com_threshold=8)
-print('Ended Computing CoMs')
-com = np.array(com)  # new CoM list
-df['CoM_sugg'] = com
+# print('Computing CoMs')
+# time_com, peak_com, com =\
+#     fig_3.com_detection(df=df, data_folder=DATA_FOLDER,
+#                         com_threshold=8)
+# print('Ended Computing CoMs')
+# com = np.array(com)  # new CoM list
+# df['CoM_sugg'] = com
 df['norm_allpriors'] = fp.norm_allpriors_per_subj(df)
 df['time_trajs'] = time_trajs
 fig_bernstein(df, task_img=TASK_IMG)
