8f746ef4884a03006ee3ab8569a552e208012a03
sara-fuentes <sarafuentessimon@gmail.com>
2023-12-02 13:34:21 +0100

    deleted useless function, improved old functions

--- utilsJ/paperfigs/reward_rate.py
+++ utilsJ/paperfigs/reward_rate.py
@@ -4,11 +4,13 @@
 Created on Wed May 17 2023
 @author: Alex Garcia-Duran
 """
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import seaborn as sns
+import itertools
 # from imp import reload
 import sys
 
@@ -94,16 +96,14 @@ PUNISHMENT = 2000
 
 
 
-def RR_core(df_data, stim, param_to_explore, value_to_explore, punishment):
+def RR_core(df_data, stim, param_to_explore, value_to_explore):
     """
     Parameters
     ----------
-    param_to_explore : str
-        name of the parameter to explore
-    value_to_explore : float
-    punishment: integer
-        penalty time for each error
-
+    param_to_explore : list
+        list with names of parameters to explore
+    value_to_explore : list
+        list with values of parameters to explore
     Returns
     -------
     rw_rate : float
@@ -115,45 +115,25 @@ def RR_core(df_data, stim, param_to_explore, value_to_explore, punishment):
                       "p_a_bound": 9, "p_1st_readout": 10,
                       "p_2nd_readout": 11, "p_leak": 12, "p_mt_noise": 13,
                       "p_MT_intercept": 14, "p_MT_slope": 15}
-    param_idx = param_idx_dict[param_to_explore]
+
+    param_idxes = [param_idx_dict[pte] for pte in param_to_explore]
     hit_model, reaction_time, com_model_detected, resp_fin, com_model,\
         _, trajs, x_val_at_updt, frst_traj_motor_time =\
         fp.simulate_model_humans(df_data, stim=stim, load_params=True,
-                                  params_to_explore=[[param_idx], [value_to_explore]])
-    rw_rate = np.sum(hit_model)/(np.sum(reaction_time) +
-              np.sum(fp.get_human_mt(df_data)) +
-              punishment*(np.sum(hit_model == 0)))
-    return rw_rate
+                                 params_to_explore=[param_idxes,
+                                                    value_to_explore])
+    sum_hit_model = np.sum(hit_model)
+    sum_rt = np.sum(reaction_time)
+    # TODO: do this for the model
+    sum_mt = np.sum([len(tr) for tr in trajs])
+    rr = 1000 * sum_hit_model / (
+        sum_rt + sum_mt + PUNISHMENT * (np.sum(hit_model == 0))
+    )
+
+    return rr, sum_hit_model, sum_rt, sum_mt
 
 
-def RR_explore(df_data, stim, param_to_explore, values_to_explore, punishment):
-    """
-    Parameters
-    ----------
-    param_to_explore : str
-        name of the parameter to explore
-    values_to_explore : list
-    punishment: integer
-        penalty time for each error
-
-    Returns
-    -------
-    rr_vect: matrix  of size: len(params_to_explore) x
-                len(values_to_explore)
-    matrix containing the reward rates for the values of the tested
-    parameters
-
-    """
-    rr_vect = np.zeros(len(values_to_explore))
-    counter = 0
-    for val in values_to_explore:
-        rw_rate = RR_core(df_data, stim, param_to_explore, val, punishment)
-        rr_vect[counter] = rw_rate
-        counter += 1
-    return rr_vect
-
-
-def RR_mat(df_data, stim, params_to_explore, values_to_explore, punishment):
+def RR_mat(df_data, stim, params_to_explore, values_to_explore):
     """
     Parameters
     ----------
@@ -173,14 +153,20 @@ def RR_mat(df_data, stim, params_to_explore, values_to_explore, punishment):
 
     """
 
-    rr_mat = []
+    rr_dict = {'rr': [], 'sum_hit_model': [], 'sum_rt': [], 'sum_mt': []}
+    list_of_values = itertools.product(*values_to_explore)
+    for i_v in list_of_values:
+        rr, sum_hit_model, sum_rt, sum_mt =\
+            RR_core(df_data=df_data, stim=stim,
+                    param_to_explore=params_to_explore,
+                    value_to_explore=i_v)
 
-    for i in range(len(params_to_explore)):
-        rr_vect = RR_explore(df_data, stim, params_to_explore[i],
-                             values_to_explore[i], punishment)
-        rr_mat.append(rr_vect)
+        rr_dict['rr'].append(rr)
+        rr_dict['sum_hit_model'].append(sum_hit_model)
+        rr_dict['sum_rt'].append(sum_rt)
+        rr_dict['sum_mt'].append(sum_mt)
 
-    return rr_mat
+    return rr_dict
 
 
 # TODO: plot_reward_rate() funtion
@@ -197,7 +183,7 @@ if __name__ == '__main__':
     #                   "p_MT_intercept": 14, "p_MT_slope": 15}
     load_params = True
     params_to_explore = ['p_a_bound', 'p_1st_readout']
-    values_to_explore = [[2, 3.61, 5], [100, 181.43, 260]]
+    values_to_explore = [np.arange(1, 11), [181.43]]
 
     df_data = fp.get_human_data(user_id=pc_name, sv_folder=SV_FOLDER)
     choice = df_data.R_response.values*2-1
@@ -213,9 +199,17 @@ if __name__ == '__main__':
     trial_index = np.empty((0))
     for j in range(len(len_task)):
         trial_index = np.concatenate((trial_index,
-                                        np.arange(len_task[j])+1))
+                                      np.arange(len_task[j])+1))
     df_data['origidx'] = trial_index
-    rr = RR_mat(df_data, stim, params_to_explore, values_to_explore, PUNISHMENT)
+    rr = RR_mat(df_data, stim, params_to_explore, values_to_explore)
     print('params_to_explore: ', params_to_explore)
     print('values_to_explore: ', values_to_explore)
     print('reward rate: ', rr)
+    # TODO: make labels
+    f, ax = plt.subplots(nrows=2, ncols=2)
+    ax = ax.flatten()
+    ax[0].plot(np.arange(1, 11), rr['rr'])
+    ax[1].plot(np.arange(1, 11), rr['sum_hit_model'])
+    ax[2].plot(np.arange(1, 11), rr['sum_rt'])
+    ax[3].plot(np.arange(1, 11), rr['sum_mt'])
+    plt.show()
